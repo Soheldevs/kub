55063f7ec06ecbf51d6a18fb2682e98e394550c6
Md Sohel Rana <89878139+Soheldevs@users.noreply.github.com>
2022-02-02 13:30:14 -0800

    Add files via upload

---
+++ js/coustom.js
@@ -0,0 +1,48 @@
+$('.banner_slick').slick({
+  dots: false,
+  speed: 300,
+  slidesToShow: 1,
+  arrows:false,
+})
+
+
+// service item
+$('.service_slick').slick({
+ 
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  centerMode:true,
+  centerPadding:false,
+  speed:1500,
+  autoplaySpeed: 2500,
+  arrows:false,
+})
+
+
+
+
+
+// instra
+ $('.instagram_slick').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows:false,
+    speed:1500,
+
+    })
+
+
+
+// jurnal
+ $('.journal_item_slick').slick({
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows:false,
+    speed:1500,
+
+    })
